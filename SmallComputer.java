0e9b6ed10760ffea0981fab1409824d35637d5bb
Ben Schoen <benshoe@gmail.com>
2016-02-03 17:10:20 +0100

    Started with GUI

--- SmallComputer.java
+++ SmallComputer.java
@@ -9,57 +9,74 @@ public class SmallComputer {
     private SmallProcessor m_processor = new SmallProcessor();
 
     public static void main(String[] args) {
+        setLookAndFeel();
         SmallComputer smallComputer = new SmallComputer();
         smallComputer.startComputer();
     }
 
-    private void startComputer() {
-        displayInformation();
-        askUserForInput();
-        stopComputer();
-    }
-
-    private void stopComputer() {
-        System.out.println("\n\nThank you for using this small computer! I hope you enjoyed it.\n" +
-                "Until next time!\n\n" +
-                "Best regards,\n\nBen Schoen");
-
-    }
-
-    private void displayInformation() {
-        System.out.println("Welcome to this small computer. By entering three digit values you are\n" +
-                "able to set and manipulate 10 registers. After each instruction I will display\n" +
-                "the value of each register.\n\n" +
-                "Instructions that are invalid will not affect the registers (e.g. dividing by 0).\n" +
-                "An instruction starting with a 0 will result in an error.\n" +
-                "If you would like to quit eht program, enter a 3-digit number starting with a '1'.");
-    }
-
-    private void askUserForInput() {
-        Scanner input = new Scanner(System.in);
-        while(true) {
-            System.out.println("\nEnter instruction: ");
-            if(m_processor.getResult() == SmallProcessor.Result.SUCCESS) {
-                String instruction = input.next();
-                if (m_processor.verifyInput(instruction)) {
-                    m_processor.processInput(instruction);
-                    printRegisters();
+    /**
+     * This method forces the Nimbus Look and Feel to be loaded
+     */
+    private static void setLookAndFeel() {
+        final UIManager.LookAndFeelInfo[] installedLookAndFeels = UIManager.getInstalledLookAndFeels();
+        for(int i = installedLookAndFeels.length - 1; i >=0; i--) {
+            if(installedLookAndFeels[i].getName().equals("Nimbus")) {
+                try {
+                    UIManager.setLookAndFeel(installedLookAndFeels[i].getClassName());
+                } catch(Exception e) {
+                    e.printStackTrace();
+                    System.out.println("Too bad, it is not possible to set the LookAndFeel to Nimbus. We'll use the default then.");
                 }
-            } else if(m_processor.getResult() == SmallProcessor.Result.ERROR) {
-                System.out.println("Error: " + m_processor.getMessage());
-                m_processor.setResult(SmallProcessor.Result.SUCCESS);
-            } else if(m_processor.getResult() == SmallProcessor.Result.STOPPED) {
-                break;
             }
         }
     }
 
-    private void printRegisters() {
-        for(int i = 0; i < m_processor.getRegisters().length; i ++) {
-            System.out.print( i + ": ");
-            System.out.printf("%03d\t", m_processor.getRegisters()[i]);
+    private void startComputer() {
+        displayInformation();
+        while(m_processor.getResult() != SmallProcessor.Result.STOPPED) {
+            askUserForInput();
         }
-        System.out.println();
+        stopComputer();
+    }
+
+    private void stopComputer() {
+        JOptionPane.showMessageDialog(null, "Thank you for using this small computer! I hope you enjoyed it.\n" +
+                "Until next time!\n\n" +
+                "Best regards,\n\nBen Schoen", "Small Computer", JOptionPane.INFORMATION_MESSAGE);
+
+    }
+
+    private void displayInformation() {
+        JOptionPane.showMessageDialog(null, "Welcome to this small computer. By entering three digit values you are\n" +
+                "able to set and manipulate 10 registers. After each instruction I will display\n" +
+                "the value of each register.\n\n" +
+                "Instructions that are invalid will not affect the registers (e.g. dividing by 0).\n" +
+                "An instruction starting with a 0 will result in an error.\n" +
+                "If you would like to quit the program, enter a 3-digit number starting with a '1'.", "Small computer", JOptionPane.INFORMATION_MESSAGE);
+    }
+
+    private void askUserForInput() {
+        String instruction = JOptionPane.showInputDialog(null, "Enter an instruction", "Small Computer", JOptionPane.QUESTION_MESSAGE);
+        if (m_processor.verifyInput(instruction)) {
+            m_processor.processInput(instruction);
+            if(m_processor.getResult() == SmallProcessor.Result.SUCCESS) {
+//                String instruction = input.next();
+                printRegisters();
+            }
+        } else if(m_processor.getResult() == SmallProcessor.Result.ERROR) {
+            JOptionPane.showMessageDialog(null, m_processor.getMessage(), "Error", JOptionPane.OK_OPTION);
+            m_processor.setResult(SmallProcessor.Result.SUCCESS);
+        }
+    }
+
+    private void printRegisters() {
+        StringBuilder registers = new StringBuilder();
+        for(int i = 0; i < m_processor.getRegisters().length; i ++) {
+            registers.append( i + ": ");
+            registers.append(String.format(String.valueOf(m_processor.getRegisters()[i]), "%03d\t"));
+        }
+        JOptionPane.showMessageDialog(null, registers, "Result", JOptionPane.OK_OPTION);
+
     }
 
 }
